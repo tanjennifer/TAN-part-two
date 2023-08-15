1fabe2eca901e7f971aeed4f2407c299e50afb0c
Jennifer <jennifertan1994@gmail.com>
2023-08-15 19:52:50 -0400

    update variable

--- script.js
+++ script.js
@@ -12,13 +12,10 @@ for (let i = 0; i < golfHole; i++) {
 }
 console.log("Score: " + totalPar + ".");
 
-
 if (coursePar === totalPar) {
     console.log(`Good job, ${userName}. Your total par was: 0`);
-    } else if (coursePar < totalPar) {
-    console.log(`Great job, ${userName}! Your total par was -${coursePar}`);
     } else if (coursePar > totalPar) {
-    console.log(`Nice try, ${userName}... Your total par was +${coursePar}`);
+    console.log(`Great job, ${userName}! Your total par was ${totalPar - coursePar}`);
+    } else if (coursePar < totalPar) {
+    console.log(`Nice try, ${userName}... Your total par was +${totalPar - coursePar}`);
 }
-
-
